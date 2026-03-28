bbdd64b09134c613cc68b8a8ee8bed492d8f927a
kombifxx <arinaghost@gmail.com>
2026-03-28 18:47:26 +0300

    fixed bugs 6

--- src/validators/posts.valid.ts
+++ src/validators/posts.valid.ts
@@ -1,27 +1,25 @@
 import { body, param } from "express-validator";
 export const postValidation = [
     body('title')
+        .exists().withMessage('Title is required')
+        .isString().withMessage('Title must be a string')
         .trim()
         .notEmpty().withMessage('Title is required')
-        .bail()
-        .isString().withMessage('Title must be string')
-        .bail()
-        .isLength({ max: 30 }).withMessage('Title max 30'),
+        .isLength({ max: 30 }),
 
-  body("shortDescription")
-    .exists()
-    .withMessage("Description cannot be empty")
-    .isString()
-    .withMessage("Description should be a string")
-    .isLength({ max: 100 })
-    .withMessage("Description should be less than 100 characters"),
+    body('shortDescription')
+        .exists().withMessage('ShortDescription is required')
+        .isString().withMessage('ShortDescription must be a string')
+        .trim()
+        .notEmpty().withMessage('ShortDescription is required')
+        .isLength({ max: 100 }),
+
     body('content')
+        .exists().withMessage('Content is required')
+        .isString().withMessage('Content must be a string')
         .trim()
         .notEmpty().withMessage('Content is required')
-        .bail()
-        .isString().withMessage('Content must be string')
-        .bail()
-        .isLength({ max: 1000 }).withMessage('Content max 1000'),
+        .isLength({ max: 1000 }),
     body('blogId')
         .exists().withMessage('blogId is required')
         .isString().withMessage('blogId must be string')
